2c386e9dcafe237bb351aa114f4f79252e3bc27f
minidonut <nycom13@gmail.com>
2020-05-03 19:31:42 +0900

    Add schema type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,63 @@
-console.log("Hello world!");
+import { command } from "yargs";
+
+
+type ValidType = "string" | "boolean" | "number" | string[];
+
+type SchemaItem = {
+  type: ValidType;
+  description?: string;
+  shared?: boolean;
+}
+
+type Schema = Record<string, SchemaItem>;
+
+
+interface ConfigType {
+  asdf: number;
+  qwde: string;
+  zxca: "asd" | "qwd" | "qwd";
+}
+
+/* type SchemaGenerator = (s: Record<string, SchemaItem>) => SchemaItem["type"] */
+
+const schema = (s: Schema) => {
+
+  return s;
+};
+
+interface Option {
+  filename: string;
+  schema: Schema;
+  profile?: string;
+  base?: string;
+}
+
+const init = ({ filename, schema, profile = "default", base = "~/.config" }: Option): any => {
+  console.log(process.argv);
+  /*
+  command("config", false, () => { }, (argv) => {
+    console.log("huh");
+    console.log(argv);
+  });
+  */
+};
+
+
+
+const cmdconfig = {
+  schema,
+  init,
+  string: "",
+  number: 0,
+  boolean: true,
+};
+
+const configSchema = cmdconfig.schema({
+  accessKey: { type: "string", description: "...", shared: true },
+  timeout: { type: "number", description: "..." },
+});
+
+const asdf = init({
+  filename: ".myappconfig",
+  schema: configSchema,
+});
