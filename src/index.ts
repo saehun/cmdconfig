98422f564322d7fcef57df659e47582c3583867e
minidonut <nycom13@gmail.com>
2020-05-06 13:45:09 +0900

    export public api

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,2 @@
-import { init } from "./init";
-import { schema } from "./schema";
-
-const configSchema = schema({
-  "accessKey": { type: "string", description: "Access key of the account" },
-  "secretAccessKey": { type: "string", description: "Secret access key of the account" },
-  "region": { type: ["us-east-2", "ap-northeast-2", "eu-west-1"], description: "Base region" },
-  "timeout": { type: "number", description: "Defualt request timeout in seconds", shared: true },
-  "cache": { type: "boolean", description: "Caching result", shared: true },
-});
-
-const asdf = init({
-  filename: ".myappconfig",
-  schema: configSchema,
-  profile: process.env.MY_APP_PROFILE,
-});
-console.log(asdf);
+export { init } from "./init";
+export { schema } from "./schema";
